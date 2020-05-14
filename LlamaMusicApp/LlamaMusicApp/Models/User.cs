33c3363b8b4e335be513c4ab6e845599e68dd438
Ahsan Farooq <ahsan.farooq@yahoo.com>
2020-05-13 21:15:40 -0700

    Essential base classes; Some sample music

---
+++ LlamaMusicApp/LlamaMusicApp/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LlamaMusicApp.Models
+{
+    public class User
+    {
+        public string Name { get; private set; }
+        public string Password { get; private set; }
+
+        public User(string name, string password)
+        {
+            Name = name;
+            Password = password;
+        }
+    }
+}
